11d09501327d26657827195bd06e3017c2fde9f8
Sajnovich <101999288+Sajnovich@users.noreply.github.com>
2026-05-01 16:33:14 -0400

    inicio de ms-carrito
    Se agrega configuración estándar, solo es el inicio del microservicio

---
+++ Ms_carrito/src/test/java/com/biblioteca/Ms_carrito/MsCarritoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biblioteca.Ms_carrito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCarritoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
